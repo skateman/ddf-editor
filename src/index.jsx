177496b49118d005aed98c281fb06379005fdb5f
Dávid Halász <skateman@skateman.eu>
2019-09-01 13:03:24 +0200

    Rename the renderer component to Editor

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { DndProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
-import Renderer from './renderer';
+import Editor from './Editor';
 import createSchema from './schema';
 
 import './style.scss';
@@ -10,7 +10,7 @@ import './style.scss';
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
-      <Renderer schema={createSchema()}/>
+      <Editor schema={createSchema()}/>
     </DndProvider>
   )
 }
