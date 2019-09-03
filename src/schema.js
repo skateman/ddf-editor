8ba0eef0ad7c9c043802fe7182f4a32459f79944
Dávid Halász <skateman@skateman.eu>
2019-09-03 07:43:45 +0200

    Introduce tabs in the default schema

--- src/schema.js
+++ src/schema.js
@@ -3,18 +3,37 @@ import { componentTypes } from '@data-driven-forms/react-form-renderer';
 const createSchema = () => ({
   fields: [
     {
-      name: 'username',
-      label: 'Username',
-      component: componentTypes.TEXT_FIELD,
-    }, {
-      name: 'password',
-      label: 'Password',
-      component: componentTypes.TEXT_FIELD,
-      type: 'password',
-    }, {
-      component: componentTypes.CHECKBOX,
-      name: 'remember',
-      label: 'Remember me',
+      component: componentTypes.TABS,
+      name: 'tabs',
+      fields: [
+        {
+          component: componentTypes.TAB,
+          name: 'tab-item-1',
+          title: 'Tab 1',
+          fields: [
+            {
+              name: 'username',
+              label: 'Username',
+              component: componentTypes.TEXT_FIELD,
+            }, {
+              name: 'password',
+              label: 'Password',
+              component: componentTypes.TEXT_FIELD,
+              type: 'password',
+            }, {
+              component: componentTypes.CHECKBOX,
+              name: 'remember',
+              label: 'Remember me',
+            }
+          ]
+        },
+        {
+          component: componentTypes.TAB,
+          name: 'tab-item-2',
+          title: 'Tab 2',
+          fields: []
+        }
+      ]
     }
   ],
 });
