b3394c11db145eebcbce2d83e116b3959f716513
Dávid Halász <skateman@skateman.eu>
2019-09-06 13:23:22 +0200

    Added switch for toggling preview and editor

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,6 +1,7 @@
-import React, { useReducer, useMemo } from "react";
+import React, { useState, useReducer, useMemo } from "react";
 import FormRender, { layoutComponents, componentTypes } from '@data-driven-forms/react-form-renderer';
 import { formFieldsMapper, layoutMapper } from '@data-driven-forms/pf3-component-mapper';
+import { Switch } from 'patternfly-react';
 import classSet from 'react-classset';
 
 import Toolbox, { toolboxFields } from './Toolbox';
@@ -12,6 +13,7 @@ import Reducer from './Reducer';
 
 export default ({...props}) => {
   const { schema:initialSchema } = props;
+  const [preview, setPreview] = useState(false);
   const [{schema, isDragging}, dispatch] = useReducer(Reducer, {
     isDragging: false,
     fieldCounter: {},
@@ -40,11 +42,14 @@ export default ({...props}) => {
   return (
     <div className="dialog-editor">
       <div className="dialog-toolbox">
+        <div className="preview-switch">
+          <Switch onText="Preview" offText="Editor" value={preview} inverse={true} onChange={() => setPreview(!preview)}/>
+        </div>
         <Toolbox dispatch={dispatch}/>
       </div>
       <div className={classSet('dialog-renderer', dragClass)}>
         <FormRender
-          formFieldsMapper={draggableFormFieldsMapper}
+          formFieldsMapper={preview ? formFieldsMapper : draggableFormFieldsMapper}
           layoutMapper={layoutMapper}
           onSubmit={() => undefined}
           schema={schema}
