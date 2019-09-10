bc243d7f0c6d0f63af19c1990ce2bf29972938d8
Dávid Halász <skateman@skateman.eu>
2019-09-10 18:06:57 +0200

    Move the drag and drop provider from the app to the editor

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useReducer, useMemo } from "react";
+import { DndProvider } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import TouchBackend from 'react-dnd-touch-backend';
 import FormRender from '@data-driven-forms/react-form-renderer';
 import { formFieldsMapper, layoutMapper } from '@data-driven-forms/pf3-component-mapper';
 import { Switch } from 'patternfly-react';
 import classSet from 'react-classset';
+
+import './style.scss';
 
 import { draggableFields } from './constants';
 import Toolbox from './Toolbox';
@@ -33,23 +38,27 @@ export default ({...props}) => {
     [draggableFields, formFieldsMapper, dispatch]
   );
 
+  const touch = 'ontouchstart' in document.documentElement;
+
   return (
     <div className="dialog-editor">
-      <div className="dialog-toolbox">
-        <div className="preview-switch">
-          <Switch onText="Preview" offText="Editor" value={preview} inverse={true} onChange={() => setPreview(!preview)}/>
+      <DndProvider backend={touch ? TouchBackend : HTML5Backend}>
+        <div className="dialog-toolbox">
+          <div className="preview-switch">
+            <Switch onText="Preview" offText="Editor" value={preview} inverse={true} onChange={() => setPreview(!preview)}/>
+          </div>
+          <Toolbox dispatch={dispatch}/>
         </div>
-        <Toolbox dispatch={dispatch}/>
-      </div>
-      <div className={classSet('dialog-renderer', isDragging ? `drag-${isDragging}` : undefined)}>
-        <FormRender
-          formFieldsMapper={preview ? formFieldsMapper : draggableFormFieldsMapper}
-          layoutMapper={layoutMapper}
-          onSubmit={() => undefined}
-          schema={schema}
-          showFormControls={false}
-        />
-      </div>
+        <div className={classSet('dialog-renderer', isDragging ? `drag-${isDragging}` : undefined)}>
+          <FormRender
+            formFieldsMapper={preview ? formFieldsMapper : draggableFormFieldsMapper}
+            layoutMapper={layoutMapper}
+            onSubmit={() => undefined}
+            schema={schema}
+            showFormControls={false}
+          />
+        </div>
+      </DndProvider>
       <div className="dialog-properties">
         <Properties schema={schema} />
       </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { DndProvider } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
-import TouchBackend from 'react-dnd-touch-backend';
 import Editor from './Editor';
 import createSchema from './schema';
 
-import './style.scss';
-
 function App() {
-  const touch = 'ontouchstart' in document.documentElement;
-
   return (
-    <DndProvider backend={touch ? TouchBackend : HTML5Backend}>
-      <Editor schema={createSchema()}/>
-    </DndProvider>
+    <Editor schema={createSchema()}/>
   )
 }
 
