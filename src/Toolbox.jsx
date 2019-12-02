0aab9f95bc897756313db416e48d25afc6571edd
Dávid Halász <skateman@skateman.eu>
2019-12-02 14:10:23 +0100

    Join the two toolbox components into a single file

--- src/Toolbox.jsx
+++ src/Toolbox.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import classSet from 'react-classset';
 
-import ToolboxField from "./ToolboxField";
+import { DraggableItem, itemTypes } from './dragAndDrop';
+
+const ToolboxField = ({ kind, title, icon, defaultSchema = {}, dispatch }) => {
+  const [, drag, preview] = DraggableItem({ type: itemTypes.INPUT, title, kind, defaultSchema }, dispatch, 'dropNew');
+
+  return (
+    <div className="toolbox-field-inner" ref={drag}>
+      <div className="icon" ref={preview}>
+        <i className={classSet(icon, 'fa-fw')}></i>
+      </div>
+      <div className="title">
+        { title }
+      </div>
+    </div>
+  )
+};
 
 const Toolbox = ({ Switch, fields, preview, dispatch }) => {
   return (
