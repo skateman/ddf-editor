07a8756d3926d12389829c7ef6ff3e7ec18ea0db
Dávid Halász <skateman@skateman.eu>
2019-12-06 21:40:55 +0100

    Export DraggableField and bump version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { DraggableItem, FakeDropZone, DropZone, itemTypes } from './dragAndDrop';
+import DraggableField from './DraggableField';
 import Editor, { ReducerContext } from './Editor';
 
-export { DraggableItem, FakeDropZone, DropZone, itemTypes, ReducerContext };
+export { DraggableItem, FakeDropZone, DropZone, itemTypes, ReducerContext, DraggableField };
 export default Editor;
